95cbd0607882d069af0633e19b861ae4fb06aa16
Jan Bielecki <jbiel@orsted.com>
2025-08-12 09:00:38 +0200

    review 2

---
+++ examples/namespace_package/main.py
@@ -0,0 +1,14 @@
+import sys
+from pprint import pprint
+
+sys.path.append(
+    "C:\\Users\\JBIEL\\Desktop\\projects\\consistent_importing\\examples\\namespace_package\\some\\path"
+)
+sys.path.append(
+    "C:\\Users\\JBIEL\\Desktop\\projects\\consistent_importing\\examples\\namespace_package\\some\\another_path"
+)
+pprint(sys.path)
+
+
+import my_package.computation
+import my_package.model
---
+++ examples/namespace_package/some/another_path/my_package/computation/__init__.py
@@ -0,0 +1,1 @@
+print("hello from another_path.my_package.computation")
